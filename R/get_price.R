1892920b3cd63c82db0f4810e8d538b00e495cb7
Christian Berntsen <christian.berntsen96@gmail.com>
2023-05-24 14:41:41 +0200

    first commit

---
+++ R/get_price.R
@@ -0,0 +1,97 @@
+#' Get Stock Price Data
+#'
+#' @description
+#' The function extends on the `getSymbols` function from the `quantmod` package,
+#' developed by Jeffrey A. Ryan, to retrieve historical closing prices from Yahoo Finance.
+#' The function provides the flexibility to retrieve historical price data for multiple assets.
+#' It allows you to choose between retrieving daily data or aggregating the data into monthly or yearly averages.
+#'
+#'
+#'
+#' @param tickers A character vector or a concatenated string specifying one or several asset symbols.
+#' @param start_date A string in the format "YYYY-MM-DD" indicating the start date of the data retrieval.
+#' @param end_date A string in the format "YYYY-MM-DD" indicating the end date of the data retrieval.
+#' @param omit_na A logical value indicating whether or not to omit missing observations. Set to TRUE to exclude any missing values from the retrieved data, or set to FALSE to include them.
+#' @param frequency A character argument specifying the frequency of the data retrieval. Accepts the values "daily", "monthly", or "yearly" to indicate the desired frequency of the retrieved data.
+#'
+#'
+#'
+#' @return Returns a data.frame() containing historical closing prices for the provided asset symbol(s).
+#' @export
+#'
+#' @author Christian André Berntsen
+#'
+#' @examples
+#'
+#' # Return daily historical closing prices for one asset ticker/symbol
+#' get_price(
+#'   tickers = "AAPL",
+#'   start_date = "2020-01-01",
+#'   end_date = "2020-05-01",
+#'   omit_na = TRUE,
+#'   frequency = "daily"
+#'             )
+#'
+#' # Return monthly historical closing prices for three asset tickers/symbols
+#' get_price(
+#'   tickers = c("AAPL", "MSFT", "TSLA"),
+#'   start_date = "2020-01-01",
+#'   end_date = "2020-05-01",
+#'   omit_na = TRUE,
+#'   frequency = "monthly"
+#'             )
+#'
+get_price <- function(tickers, start_date, end_date, omit_na = FALSE, frequency = "daily") {
+
+  start_date <- as.Date(start_date)
+  end_date <- as.Date(end_date)
+
+  valid_frequencies <- c("daily", "monthly", "yearly")
+  if (!(frequency %in% valid_frequencies)) {
+    warning("Invalid frequency. Please use 'daily', 'monthly', or 'yearly'.")
+    return(NULL)
+  }
+
+  dataframes <- list()
+
+  for (ticker in tickers) {
+    stock_data <- quantmod::getSymbols(ticker,
+                                       from = start_date,
+                                       to = end_date,
+                                       auto.assign = FALSE)
+
+    price_col <- paste0(ticker,
+                        ".Close")
+
+    df <- data.frame(date = zoo::index(stock_data),
+                     Close = zoo::coredata(stock_data[, price_col]))
+
+    colnames(df)[2] <- ticker
+
+    dataframes[[ticker]] <- df
+  }
+
+  merged_df <- Reduce(function(x, y) merge(x, y,
+                                           by = "date",
+                                           all = TRUE),
+                      dataframes)
+
+  if (omit_na) {
+    merged_df <- merged_df[complete.cases(merged_df), ]
+  }
+  rownames(merged_df) <- NULL
+
+  if (frequency == "monthly") {
+    # Set the date to the first day of each month
+    merged_df$date <- as.Date(format(merged_df$date, "%Y-%m-01"))
+    merged_df <- aggregate(. ~ date, data = merged_df, FUN = mean)
+  } else if (frequency == "yearly") {
+    # Set the date to the first day of each year
+    merged_df$date <- as.Date(format(merged_df$date, "%Y-01-01"))
+    merged_df <- aggregate(. ~ date, data = merged_df, FUN = mean)
+  }
+
+  return(merged_df)
+}
+
+
---
+++ R/returns.R
@@ -0,0 +1,77 @@
+#' Calculate Log Or Simple Returns
+#'
+#' @description
+#' This function calculates log or simple returns for a data frame.
+#' It is designed to be used with the `get_price()` function from the `assetr` package,
+#' but can also be applied to any data frame that has a date column in the first column and prices in the remaining columns.
+#'
+#' @param stock_price A data.frame() containing dates in the first column and stock prices in the remaining columns.
+#' @param type Type of returns to calculate: "log" for logarithmic returns and "simple" for simple returns.
+#'
+#' @return Returns a data.frame() containing historical closing return observations for the provided data.frame()
+#' @export
+#'
+#' @author Christian André Berntsen
+#'
+#' @examples
+#'
+#'# Obtaining historical price data
+#' df_prices <- get_price(c("AAPL", "TSLA"),
+#'                     "2017-01-01",
+#'                     "2023-01-01",
+#'                     omit_na = T,
+#'                     frequency = "daily")
+#'
+#' # Calculate log returns
+#' df_returns <- returns(prices, type = "log")
+#'
+returns <- function(get_price, type = c("log", "simple")) {
+
+  if (!is.data.frame(get_price)) {
+    message(paste0("Input data must be of type dataframe, and have a date column in the first row, or be of type",
+                   "get_price, or be",
+                   "from the assetr package"))
+    return()
+  }
+  # If only one price variable
+  if (ncol(get_price) == 2){
+
+    if (type == "simple") {
+      returns <- head(as.vector(suppressWarnings(diff(prices[, 2]) / lag(prices[, 2]))), -1)
+    }
+    if (type == "log") {
+      returns <- diff(log(get_price[,2]))
+    }
+    dates <- get_price[-1, 1]
+    return_data <- data.frame(dates, returns)
+    colnames(return_data)[2] <- colnames(get_price)[2]
+    return(return_data)
+
+    # If more than one price variable
+  } else if (ncol(get_price) > 2){
+
+    if (type == "simple") {
+      suppressed_returns <- suppressWarnings({
+        sapply(get_price[, -1], function(col) {
+          lagged_col <- lag(col)
+          if (is.null(lagged_col)) return(NA)  # Return NA for the first element
+          diff(col) / lagged_col
+        })
+      })
+
+      returns <- suppressed_returns[-1, , drop = FALSE]  # Remove the first row
+
+    } else if (type == "log") {
+      returns <- apply(log(get_price[, -1]), 2, diff)
+    } else {
+      message("Invalid type. Allowed values are 'log' or 'simple'.")
+      return()
+    }
+    dates <- get_price[-1, 1]
+    return_data <- data.frame(dates, returns)
+
+    return(return_data)
+  }
+}
+
+
