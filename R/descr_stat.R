e57eeb0c2b7426da20dbfda3d37eb403dd7f8ac8
Christian Berntsen <christian.berntsen96@gmail.com>
2023-06-15 10:12:38 +0200

    update 15.06.2023

--- R/descr_stat.R
+++ R/descr_stat.R
@@ -3,15 +3,22 @@
 #' @param data A data.frame() containing dates in the first column and numbers in the remaining columns
 #' @param start_date An optional string in the format "YYYY-MM-DD" indicating the start date of the data retrieval
 #' @param end_date An optional string in the format "YYYY-MM-DD" indicating the end date of the data retrieval
+#' @param day_count Annualised measure for return and standard deviation -
 #'
 #' @return Returns a data.frame() that includes a range of descriptive statistics for the given input data.frame()
 #' @export
 #'
 #' @examples
 #'
-#' df <- descr_stat(prices, start_date = "2022-01-01", end_date = "2023-01-01")
+#' df <- descr_stat(prices,
+#'                  start_date = "2022-01-01",
+#'                  end_date = "2023-01-01",
+#'                  day_count = 252)
 #'
-descr_stat <- function(data, start_date = NULL, end_date) {
+descr_stat <- function(data,
+                       start_date = data[1,1],
+                       end_date = data[,1][nrow(data)],
+                       day_count = 1) {
 
   n <- ncol(data)
 
@@ -27,8 +34,8 @@ descr_stat <- function(data, start_date = NULL, end_date) {
 
     df <- data.frame(
       name = colnames(filtered_data)[2:ncol(filtered_data)],
-      mean = mean_val,
-      stdDev = stdDev_val,
+      mean = mean_val*day_count,
+      stdDev = stdDev_val*sqrt(day_count),
       sharpe = mean_val / stdDev_val,
       min = min(filtered_data[, 2]),
       max = max(filtered_data[, 2]),
@@ -41,25 +48,21 @@ descr_stat <- function(data, start_date = NULL, end_date) {
     df[, 2:8] <- round(df[, 2:8], 2)
 
     return(df)
+
   } else {
+
     # Calculate mean
-    mean_values <- colMeans(filtered_data[, -1], na.rm = TRUE)
-
+    mean_values <- colMeans(filtered_data[, -1], na.rm = TRUE)*day_count
     # Calculate standard deviation
-    std_dev_values <- apply(filtered_data[, -1], 2, sd, na.rm = TRUE)
-
+    std_dev_values <- apply(filtered_data[, -1], 2, sd, na.rm = TRUE)*sqrt(day_count)
     # Calculate Sharpe ratio
     sharpe_ratio <- mean_values / std_dev_values
-
     # Calculate minimum
     min_values <- apply(filtered_data[, -1], 2, min, na.rm = TRUE)
-
     # Calculate maximum
     max_values <- apply(filtered_data[, -1], 2, max, na.rm = TRUE)
-
     # Calculate kurtosis using moments package
     kurtosis_values <- moments::kurtosis(filtered_data[, -1], na.rm = TRUE)
-
     # Calculate skewness using moments package
     skewness_values <- moments::skewness(filtered_data[, -1], na.rm = TRUE)
 
@@ -78,13 +81,10 @@ descr_stat <- function(data, start_date = NULL, end_date) {
     )
 
     df[, 2:8] <- round(df[, 2:8], 2)
+
     return(df)
+
   }
 }
-
-
-
-
-
 
 
